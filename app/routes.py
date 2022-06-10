2247123ddf29afbda9df3980f5c1b41558558e8d
mniroz <max.zorin.2022@gmail.com>
2022-06-10 13:53:21 -0700

    added the for Jinja block

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,23 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Test User'},
+            'body': 't is a long established fact that a reader will be'
+                    ' distracted by the readable content of a page when looking at its layout'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
